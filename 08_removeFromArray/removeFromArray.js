d254ad13c4e01d10f29637ef8ab9c06bba671e2e
crousty24-bit <242641564+crousty24-bit@users.noreply.github.com>
2025-12-09 18:00:53 +0100

    add while to removes same multiples same values

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,8 +1,7 @@
 const removeFromArray = (arr, ...num) => { 
     for(value of num) {
-        let index = arr.indexOf(value); 
-        if (index !== -1) {
-            arr.splice(index, 1);
+        while(arr.indexOf(value) !== -1) {
+            arr.splice(arr.indexOf(value), 1);
         } 
     }
     return arr;
@@ -10,5 +9,7 @@ const removeFromArray = (arr, ...num) => {
 console.log(removeFromArray([1, 2, 2, 3, 4, 5, 6, 7, 8, 9], 2, 4, 3, 7, 10))
 // add ...parameter turn into array of values
 // for (item of arr) "value" is just a name, contains the values of arr num
+// while to remove multiple of the same value
+
 // Do not edit below this line
 module.exports = removeFromArray;
