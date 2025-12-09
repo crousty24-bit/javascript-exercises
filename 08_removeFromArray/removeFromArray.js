955504b69c90252260cc63e4c8fe3c2d97f153b9
crousty24-bit <242641564+crousty24-bit@users.noreply.github.com>
2025-12-09 17:35:47 +0100

    add for of loop to remove multiple values

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,11 +1,13 @@
-const removeFromArray = (arr, num) => {
-    let index = arr.indexOf(num); 
-    if (index !== -1) {
-        arr.splice(index, 1);
+const removeFromArray = (arr, ...num) => {
+    for(value of num) {
+        let index = arr.indexOf(value); 
+        if (index !== -1) {
+            arr.splice(index, 1);
+        } 
     }
     return arr;
 }
-console.log(removeFromArray([1, 2, 3, 4], 4))
+console.log(removeFromArray([1, 2, 3, 4, 5, 6, 7, 8, 9], 2, 4, 3, 7))
 
 // Do not edit below this line
 module.exports = removeFromArray;
