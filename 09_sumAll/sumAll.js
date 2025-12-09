828351280a4fbb0276897ab2eeedcc99c00d5fb4
crousty24-bit <242641564+crousty24-bit@users.noreply.github.com>
2025-12-09 18:50:37 +0100

    add for loop to sum num1 and num 2

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
-};
+const sumAll = (num1, num2) => {
+    let sum = 0;
+    let start = Math.min(num1, num2);
+    let end = Math.max(num1, num2);
+    for (let i = start ; i <= end; i++) {
+        sum += i;
+    } return sum;
+}
+console.log(sumAll("123", 1))
 
 // Do not edit below this line
 module.exports = sumAll;
