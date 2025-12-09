ea01c814353a11ad1ad48e30833710b6eae38f4c
crousty24-bit <242641564+crousty24-bit@users.noreply.github.com>
2025-12-09 19:05:34 +0100

    add if NaN or a negative num then returns 'error'

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -2,11 +2,19 @@ const sumAll = (num1, num2) => {
     let sum = 0;
     let start = Math.min(num1, num2);
     let end = Math.max(num1, num2);
+    if (typeof num1 !== "number" || typeof num2 !== "number") {
+        return 'ERROR'
+    }
     for (let i = start ; i <= end; i++) {
-        sum += i;
+        if (num1 < 0 || num2 < 0) {
+            return 'ERROR'
+        }
+        else {
+            sum += i;
+        } 
     } return sum;
 }
-console.log(sumAll("123", 1))
+console.log(sumAll("caca", 4))
 
 // Do not edit below this line
 module.exports = sumAll;
