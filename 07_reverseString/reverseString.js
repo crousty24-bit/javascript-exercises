a46209e5831a7ac8d1828396ea0e3277b60de227
crousty24-bit <242641564+crousty24-bit@users.noreply.github.com>
2025-12-09 14:54:50 +0100

    define split, reverse, join methods in function

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversed = "";
+    string.split(); Array.reverse(); Array.join(' ');
+    return reversed;
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
