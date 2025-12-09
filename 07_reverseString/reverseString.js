72db02e2820c5c43f02074eaafd116be26a64d14
crousty24-bit <242641564+crousty24-bit@users.noreply.github.com>
2025-12-09 15:18:31 +0100

    add 3 methods to narrow func to return reverse working as intended

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,9 +1,12 @@
-const reverseString = function(string) {
-    let reversed = "";
-    string.split(); Array.reverse(); Array.join(' ');
+const reverseString = (string) => {
+    let str = string.split(''); 
+    let arr = str.reverse();
+    let reversed = arr.join('');
     return reversed;
-};
-
+ 
+} 
+  
+console.log(reverseString('hello there'));
 
 
 // Do not edit below this line
