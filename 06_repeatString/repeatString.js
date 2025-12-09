6f39cc3f8defd41dfeb31ea3e5f000b5dac86ffe
crousty24-bit <242641564+crousty24-bit@users.noreply.github.com>
2025-12-09 11:04:04 +0100

    add initializer, 'error' condition, for loop

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+function repeatString(string, num) {
+    let repeatedString = "";
+    if (num < 0 ) {
+        return 'ERROR'
+    }
+    for (let i = 0; i < num ; i++) {
+        
+        
 };
+
+}
+
+
+//const repeatString
 
 // Do not edit below this line
 module.exports = repeatString;
