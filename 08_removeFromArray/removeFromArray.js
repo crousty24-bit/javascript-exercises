35d58278db810bab238d44c1a841a32c1d4a8b16
crousty24-bit <242641564+crousty24-bit@users.noreply.github.com>
2025-12-09 17:41:42 +0100

    add comments to remember where I did struggle the correct use and syntax of for of loop the use of rest parameters(...num)

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,4 @@
-const removeFromArray = (arr, ...num) => {
+const removeFromArray = (arr, ...num) => { 
     for(value of num) {
         let index = arr.indexOf(value); 
         if (index !== -1) {
@@ -7,7 +7,8 @@ const removeFromArray = (arr, ...num) => {
     }
     return arr;
 }
-console.log(removeFromArray([1, 2, 3, 4, 5, 6, 7, 8, 9], 2, 4, 3, 7))
-
+console.log(removeFromArray([1, 2, 2, 3, 4, 5, 6, 7, 8, 9], 2, 4, 3, 7, 10))
+// add ...parameter turn into array of values
+// for (item of arr) "value" is just a name, contains the values of arr num
 // Do not edit below this line
 module.exports = removeFromArray;
