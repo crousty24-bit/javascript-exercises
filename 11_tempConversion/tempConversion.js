53f8e07e19ea6ba1f72226023ae8b312480c880f
crousty24-bit <242641564+crousty24-bit@users.noreply.github.com>
2025-12-09 21:19:58 +0100

    add var basic conversion operation to both func return a Number round up to 1 decimal with

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = (num) => {
+    let celsius = (num-32) * 5/9; // applied raw math conv from wiki into code
+    return Number(celsius.toFixed(1)) // Number() convert a string
+}
+console.log(convertToCelsius(68.5))
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = (num) => {
+    let fahrenheit = (num * 9/5 + 32);
+    return Number(fahrenheit.toFixed(1)) // toFixed(1) will round up to 1 decimal
+}
+console.log(convertToFahrenheit(20.5))
 
 // Do not edit below this line
 module.exports = {
