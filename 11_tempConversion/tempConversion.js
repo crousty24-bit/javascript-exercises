ab003ac6bcc2f8b6c9eb5d7a4b93789ebf077981
crousty24-bit <242641564+crousty24-bit@users.noreply.github.com>
2025-12-11 14:47:59 +0100

    add comments after review code and solution

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,6 +1,6 @@
 const convertToCelsius = (num) => {
     let celsius = (num-32) * 5/9; // applied raw math conv from wiki into code
-    return Number(celsius.toFixed(1)) // Number() convert a string
+    return Number(celsius.toFixed(1)) // Number() convert a string into number
 }
 console.log(convertToCelsius(68.5))
 
@@ -10,6 +10,9 @@ const convertToFahrenheit = (num) => {
 }
 console.log(convertToFahrenheit(20.5))
 
+
+// ok with this method, but need to convert string to number
+// still it's pretty clean
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
