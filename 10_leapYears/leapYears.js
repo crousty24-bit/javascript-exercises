e95da2848c82271b43ea6a5e21f8c2a2d8695d3d
crousty24-bit <242641564+crousty24-bit@users.noreply.github.com>
2025-12-11 14:24:45 +0100

    change else if to check 2 conditions with &&

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,16 +1,15 @@
 const leapYears = (year) => {
     if (year % 400 === 0)  {
         return true 
-    } else if (year % 100 === 0) {
-        return false 
-    } else if (year % 4 === 0) {
-        return true 
+    } else if (year % 4 === 0 && year % 100 !== 0) {
+        return true
     } else {
         return false
     }
 }
 console.log(leapYears(1984))
-// will do another version with && operator
+// new logic version with && to regroup conditions
+// works as intended in a clear step by step style
 
 // Do not edit below this line
 //module.exports = leapYears;
