40c5e0f061ccfa5e41ef722a0a38d78f7800b692
crousty24-bit <242641564+crousty24-bit@users.noreply.github.com>
2025-12-11 12:16:29 +0100

    check if conditions at the top of func reorder logic and lines now code is clearer

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,25 +1,26 @@
 const sumAll = (num1, num2) => {
-    let sum = 0;
-    let start = Math.min(num1, num2);
-    let end = Math.max(num1, num2);
     if (typeof num1 !== "number" || typeof num2 !== "number") {
         return 'ERROR: NaN'
     }
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) { 
+        return 'ERROR : not an Integer'
+    }
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR : negative number'
+    }
+    
+    let sum = 0;
+    let start = Math.min(num1, num2);
+    let end = Math.max(num1, num2);
+    
     for (let i = start ; i <= end; i++) {
-        if (num1 < 0 || num2 < 0) {
-            return 'ERROR : negative number'
-        }
-        else if (Number.isInteger(num1) && Number.isInteger(num2)) {
-            sum += i;
-        }
-        else {
-            return 'ERROR : not an integer'
-        }
-    } return sum;
+        sum += i;
+    } 
+    return sum;
 }
 console.log(sumAll(1, 10))
 // adding Number.isInteger if True, execute sum
-// last condition 'else' return 'error' whenever its not an integer
+
 // now code works as intended and throw an error according to
 // a missing condition
 
