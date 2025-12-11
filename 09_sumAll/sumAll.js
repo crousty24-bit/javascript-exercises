669b5655d4806b2cc9ef1357534dc4dab8eb43e5
crousty24-bit <242641564+crousty24-bit@users.noreply.github.com>
2025-12-11 11:50:52 +0100

    add second condition to check if is an integer add last conditon to return error msg if not we have 3 conditions to return specific error msg

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -3,18 +3,21 @@ const sumAll = (num1, num2) => {
     let start = Math.min(num1, num2);
     let end = Math.max(num1, num2);
     if (typeof num1 !== "number" || typeof num2 !== "number") {
-        return 'ERROR'
+        return 'ERROR: NaN'
     }
     for (let i = start ; i <= end; i++) {
         if (num1 < 0 || num2 < 0) {
-            return 'ERROR'
+            return 'ERROR : negative number'
+        }
+        else if (Number.isInteger(num1) && Number.isInteger(num2)) {
+            sum += i;
         }
         else {
-            sum += i;
-        } 
+            return 'ERROR : not an integer'
+        }
     } return sum;
 }
-console.log(sumAll(2.5, 4))
+console.log(sumAll(1, 10))
 // missing condition if is non-integer parameter 'error'
 // come back to it later
 
