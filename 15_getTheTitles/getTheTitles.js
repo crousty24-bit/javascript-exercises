62294ec3c0d138a2c645b549b4bf46f9bda9f204
crousty24-bit <242641564+crousty24-bit@users.noreply.github.com>
2025-12-16 15:45:36 +0100

    add second func to get authors exercise complete, run test passed

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: 'Crimes & Passions',
+    author: 'Sarah Sour'
+  },
+  {
+    title: 'The Haunted Door',
+    author: 'Steve Boloni'
+  }
+]
 
-};
+const getTheTitles = (books) => books.map(book => book.title);
+
+console.log(getTheTitles(books));
+
+// add same func for author :
+const getTheAuthors = (books) => books.map(writter => writter.author);
+
+console.log(getTheAuthors(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
