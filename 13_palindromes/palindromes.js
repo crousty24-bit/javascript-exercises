4ae2c248895ffea20417162394699a6b1c0fca1b
crousty24-bit <242641564+crousty24-bit@users.noreply.github.com>
2025-12-16 12:16:01 +0100

    add func, completed exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = (string) => {
+    let normalized = string.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let reversed = normalized.split('').reverse().join('');
+    return normalized === reversed;
 };
 
 // Do not edit below this line
