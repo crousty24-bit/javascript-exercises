e60a6214264f44f61f630cd9779be561b2c8d855
crousty24-bit <242641564+crousty24-bit@users.noreply.github.com>
2025-12-16 14:45:37 +0100

    add func and comments

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n);
+    if (isNaN(n) || n < 0) return "OOPS"; // handles strings, else error 
 
+    const first = 0; // 0e terme
+    const second = 1; // 1st terme
+    // conditions to make sure accepts any sequence : 
+    // (0, 1) (1, 1) or (1, 2)
+    if (n === 0) return first;
+    if (n === 1) return second;
+
+    let a = first;
+    let b = second;
+    let c;
+    // loop starts at the second :
+    for (let i = 2; i <= n; i++) {
+        c = a + b;
+        a = b ;
+        b = c ;
+    }
+    return c;
 };
 
 // Do not edit below this line
