0f4cfe80470014c3af4778822a6ef9a7391acbec
crousty24-bit <242641564+crousty24-bit@users.noreply.github.com>
2025-12-16 11:44:07 +0100

    complete all func use of arrow func the more I can

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,28 @@
-const add = function() {
-	
+const add = (a, b) => a + b ;
+
+const subtract = (a, b) => a - b;
+
+const sum = (arr) => {
+	let sum = arr.reduce((total, current) => total + current, 0);
+  return sum
 };
 
-const subtract = function() {
-	
+const multiply = (arr) => {
+  let result = arr.reduce((total, current) => total * current, 1);
+  return result
 };
 
-const sum = function() {
-	
-};
+const power = (a, b) => a ** b;
 
-const multiply = function() {
-
+const factorial = (a) => {
+  if (a === 0) return 1;
+  let resultFacto = 1;
+  for(let i = 1; i <= a ; i++)
+    resultFacto *= i;
+	return resultFacto;
 };
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+// arrow version : 
+const facto = (a) => a === 0 ? 1 : a * facto(a - 1);
 
 // Do not edit below this line
 module.exports = {
